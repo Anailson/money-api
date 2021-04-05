f101b096da5c13659824dcc8b3a3f3bccb1724b0
anailson ribeiro <arsantos315@gmail.com>
2021-04-05 15:38:25 -0300

    Bean Validation

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,10 +38,10 @@ public class CategoriaResource {
 		return (List<Categoria>) categoriaRepository.findAll();
 	}
 	
-	/*SALVAR NOVOS REGISTROS*/
+	/*----------------------SALVAR NOVOS REGISTROS----------------------------*/
 	@PostMapping
 	//@ResponseStatus(HttpStatus.CREATED) //STATUS 201 CREATED
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria , HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@Valid  @RequestBody Categoria categoria , HttpServletResponse response) {
 		
 		Categoria categoriaSalvar = categoriaRepository.save(categoria);
 		URI uri = ServletUriComponentsBuilder
