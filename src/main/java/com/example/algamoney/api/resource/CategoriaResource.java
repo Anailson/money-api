6467875faa69511b140457def8c72c3c37f76872
anailson ribeiro <arsantos315@gmail.com>
2021-04-06 14:21:45 -0300

    Eventos add header Location

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,18 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
@@ -29,6 +27,9 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	/*LISTAR*/
 	@GetMapping
@@ -44,13 +45,14 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criar(@Valid  @RequestBody Categoria categoria , HttpServletResponse response) {
 		
 		Categoria categoriaSalvar = categoriaRepository.save(categoria);
-		URI uri = ServletUriComponentsBuilder
+		/*URI uri = ServletUriComponentsBuilder
 		.fromCurrentRequestUri()
 		.path("/{codigo}")
 		.buildAndExpand(categoriaSalvar.getCodigo())
-		.toUri();
+		.toUri();*/
 		
-		return ResponseEntity.created(uri).body(categoriaSalvar);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalvar.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalvar);
 		
 	}
 	/*BUSCANDO PELO CODIGO*/
